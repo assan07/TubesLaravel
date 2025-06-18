a1ad1e3b0810f14c003ba097bdd0faa2e9896910
assan07 <hasanwkl04@gmail.com>
2025-06-18 14:24:43 +0800

    edit page admin/ kelola-data-akun

--- resources/views/admin/kelolaDataBerkas.blade.php
+++ resources/views/admin/kelolaDataBerkas.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+@extends('layouts.admin.app')
+
+@section('title', 'Kelola Data Berkas - Sistem Asrama Unidayan')
+@section('css')
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/mahasiswa/informasidatakamar.css') }}"> --}}
+
+@section('main-content')
+    <div class="container-fluid">
+        
+    </div>
+    {{-- Push JS --}}
+    @push('scripts')
+        {{-- <script src="{{ asset('assets/js/mahasiswa/login_mahasiswa.js') }}"></script> --}}
+    @endpush
+
+@endsection
--- resources/views/admin/kelolaDataPenghuni.blade.php
+++ resources/views/admin/kelolaDataPenghuni.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+@extends('layouts.admin.app')
+
+@section('title', 'Kelola Data Penghuni - Sistem Asrama Unidayan')
+@section('css')
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/mahasiswa/informasidatakamar.css') }}"> --}}
+
+@section('main-content')
+    <div class="container-fluid">
+        
+    </div>
+    {{-- Push JS --}}
+    @push('scripts')
+        {{-- <script src="{{ asset('assets/js/mahasiswa/login_mahasiswa.js') }}"></script> --}}
+    @endpush
+
+@endsection
