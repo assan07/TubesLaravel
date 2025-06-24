7a8dc2ddcaa6b09fee617df3423db48d0d60a772
Paiiiiiiii <fariskatobengke@gmail.com>
2025-06-24 21:19:00 +0800

    Back end data kamar done

---
+++ database/migrations/2025_06_24_113818_create_rooms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kamar');
+            $table->string('no_kamar')->unique();
+            $table->string('lokasi_kamar');
+            $table->enum('jenis_kamar', ['laki-laki', 'perempuan']);
+            $table->enum('status', ['tersedia', 'diisi', 'maintenance'])->default('tersedia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
