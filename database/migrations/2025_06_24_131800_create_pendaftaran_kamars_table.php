d6def01779ef632b0b4fd0c80c137fb61c14d3f7
assan07 <hasanwkl04@gmail.com>
2025-06-26 20:47:34 +0800

    WIP: Perubahan lokal sebelum pull

--- database/migrations/2025_06_24_131800_create_pendaftaran_kamars_table.php
+++ database/migrations/2025_06_24_131800_create_pendaftaran_kamars_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('pendaftaran_kamars', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->after('id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('room_id');
             $table->string('nama');
             $table->string('nim')->unique();
@@ -19,7 +19,6 @@ return new class extends Migration {
             $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
             $table->date('tanggal_pendaftaran');
             $table->timestamps();
-
             $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
         });
     }
--- resources/views/admin/dataKamar/kelolaDataKamar.blade.php
+++ resources/views/admin/dataKamar/kelolaDataKamar.blade.php
@@ -74,48 +74,83 @@
                  </div>
 
                  {{-- ======================= KAMAR PEREMPUAN ======================= --}}
-                 <div class="room-girl card col-sm-12 col-md-12 d-flex flex-column p-4 gap-3">
+                 <div class="room-girl card col-sm-12 col-md-12 col-xl-12 d-flex flex-column flex-wrap p-4 gap-2">
                      <h1><strong>Kamar Perempuan</strong></h1>
-                     <div class="card-room-girl row-lg-12 col-md-12 col-sm-12 d-flex gap-3">
+                     <div class="card-room-girl row-lg-12 col-md-12 col-sm-12 d-flex gap-3 w-100">
 
                          {{-- Total: tanpa filter --}}
-                         <a href="{{ url('kelola-data-kamar/data-kamar/perempuan') }}"
-                             class="card-room bg-primary w-100 rounded d-flex flex-column align-items-center p-2">
-                             <div
-                                 class="sub-card bg-white w-100 rounded d-flex align-items-center justify-content-center p-2 text-center">
-                                 <h1 style="font-weight: bolder">{{ $data['perempuan']['total'] }}</h1>
+                         <a href="{{ url('kelola-data-kamar/data-kamar/perempuan') }}" class="col-xl-3 col-md-6">
+                             <div class="card border-left-primary shadow h-100">
+                                 <div class="card-body">
+                                     <div class="row no-gutters align-items-center">
+                                         <div class="col mr-2">
+                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                 Total
+                                             </div>
+                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                 {{ $data['perempuan']['total'] }}</div>
+                                         </div>
+
+                                     </div>
+                                 </div>
                              </div>
-                             <strong class="text-white mt-1">Total</strong>
                          </a>
 
                          {{-- Tersedia --}}
                          <a href="{{ url('kelola-data-kamar/data-kamar/perempuan?status=tersedia') }}"
-                             class="card-room bg-success w-100 rounded d-flex flex-column align-items-center p-2">
-                             <div
-                                 class="sub-card bg-white w-100 rounded d-flex align-items-center justify-content-center p-2 text-center">
-                                 <h1 style="font-weight: bolder">{{ $data['perempuan']['tersedia'] }}</h1>
+                             class="col-xl-3 col-md-6">
+                             <div class="card border-left-success shadow h-100">
+                                 <div class="card-body">
+                                     <div class="row no-gutters align-items-center">
+                                         <div class="col mr-2">
+                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                 Tersedia
+                                             </div>
+                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                 {{ $data['perempuan']['tersedia'] }}</div>
+                                         </div>
+
+                                     </div>
+                                 </div>
                              </div>
-                             <strong class="text-white mt-1">Tersedia</strong>
                          </a>
 
                          {{-- Diisi --}}
                          <a href="{{ url('kelola-data-kamar/data-kamar/perempuan?status=diisi') }}"
-                             class="card-room bg-info w-100 rounded d-flex flex-column align-items-center p-2">
-                             <div
-                                 class="sub-card bg-white w-100 rounded d-flex align-items-center justify-content-center p-2 text-center">
-                                 <h1 style="font-weight: bolder">{{ $data['perempuan']['diisi'] }}</h1>
+                             class="col-xl-3 col-md-6">
+                             <div class="card border-left-info shadow h-100">
+                                 <div class="card-body">
+                                     <div class="row no-gutters align-items-center">
+                                         <div class="col mr-2">
+                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                 Diisi
+                                             </div>
+                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                 {{ $data['perempuan']['diisi'] }}</div>
+                                         </div>
+
+                                     </div>
+                                 </div>
                              </div>
-                             <strong class="text-white mt-1">Diisi</strong>
                          </a>
 
                          {{-- Maintenance --}}
                          <a href="{{ url('kelola-data-kamar/data-kamar/perempuan?status=maintenance') }}"
-                             class="card-room bg-warning w-100 rounded d-flex flex-column align-items-center p-2">
-                             <div
-                                 class="sub-card bg-white w-100 rounded d-flex align-items-center justify-content-center p-2 text-center">
-                                 <h1 style="font-weight: bolder">{{ $data['perempuan']['maintenance'] }}</h1>
+                             class="col-xl-3 col-md-6">
+                             <div class="card border-left-warning shadow h-100">
+                                 <div class="card-body">
+                                     <div class="row no-gutters align-items-center">
+                                         <div class="col mr-2">
+                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                 Maintenance
+                                             </div>
+                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                 {{ $data['perempuan']['maintenance'] }}</div>
+                                         </div>
+
+                                     </div>
+                                 </div>
                              </div>
-                             <strong class="text-white mt-1">Maintenance</strong>
                          </a>
 
                      </div>
